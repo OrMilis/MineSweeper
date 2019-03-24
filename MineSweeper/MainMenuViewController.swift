5332d9d37148cb9f6872107feeb68b34219e8e1c
Or Milis <ormilis@Ors-iMac.local>
2019-03-24 16:32:25 +0200

    TestView

---
+++ MineSweeper/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Or Milis on 24/03/2019.
+//  Copyright © 2019 Or Milis. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextBox: UITextField!
+    @IBOutlet weak var applyNameBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func nameBtnOnClick(_ sender: Any) {
+        guard let nickname = nameTextBox.text else { return; };
+        nameLabel.text = nickname;
+    }
+    
+}
+
