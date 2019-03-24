87ea1e60fe0430ba3ddad0b82b740e9632714b9c
Or Milis <ormilis@Ors-iMac.local>
2019-03-24 13:57:54 +0200

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Or Milis on 24/03/2019.
+//  Copyright © 2019 Or Milis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
