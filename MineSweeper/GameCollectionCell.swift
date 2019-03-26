a8e905010ff6f47937b6e066f67ff0a4f0230322
Or Milis <ormilis@Ors-iMac.local>
2019-03-26 15:57:51 +0200

    Cell Added

---
+++ MineSweeper/GameCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  GameCollectionCell.swift
+//  MineSweeper
+//
+//  Created by Or Milis on 26/03/2019.
+//  Copyright © 2019 Or Milis. All rights reserved.
+//
+
+import UIKit
+
+class GameCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellText: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+}
